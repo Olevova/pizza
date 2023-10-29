edba77c2eccfbca3651e7b841c3f66671f652243
Olevova <olevova1983@gmail.com>
2023-10-29 13:39:29 +0200

    new2

--- app/cart/layout.jsx
+++ app/cart/layout.jsx
@@ -1,29 +1,29 @@
 'use client'
 
 import { useSelector, useDispatch } from "react-redux";
-import {Provider} from 'react-redux';
-import store from '@/redux/store';
+// import {Provider} from 'react-redux';
+// import store from '@/redux/store';
 import React from "react";
 import Link from "next/link";
 import {removeAllPizzasFromCart} from '@/redux/cartSlice';
 export default function CartLayout({ children }) {
-    const cartItem = useSelector((state) => state.cart);
-    const getTotalPrice = () => {
-        return cartItem.reduce(
-            (accumulator, item) => accumulator + item.quantity * item.price,
-            0
-        );
-    };
-    const dispatch = useDispatch();
+    // const cartItem = useSelector((state) => state.cart);
+    // const getTotalPrice = () => {
+    //     return cartItem.reduce(
+    //         (accumulator, item) => accumulator + item.quantity * item.price,
+    //         0
+    //     );
+    // };
+    // const dispatch = useDispatch();
     const cleanCart = () => dispatch(removeAllPizzasFromCart());
 
     return (
-        <Provider store={store} >
+        // <Provider store={store} >
 
             <main className="flex flex-col flex-1 p-4">
                 <h1 className="text-2xl flex justify-center font-extrabold mb-8">Cart Page</h1>
-
-            {cartItem.length === 0 ? (
+                {/*cartItem.length*/}
+                {1 === 0 ? (
                 <div className="text-center">
                 <h1>Your Cart is Empty!</h1>
                 <Link href='/' className= 'text-[#CA0123] hover:text-[#660000]'>Please, choose pizza here</Link>
@@ -38,12 +38,12 @@ export default function CartLayout({ children }) {
                         <div className='flex-1 text-center text-xs font-semibold border-b-2 border-black pb-2 uppercase hidden md:flex justify-center'>Actions</div><div className='flex-1 text-center text-xs font-semibold border-b-2 border-black pb-2 uppercase'>Total Price</div>
                     </div>
                     {children}
-                    <h2 className="text-2xl mb-4 text-center">Total: UAH {getTotalPrice()}</h2>
+                    {/*<h2 className="text-2xl mb-4 text-center">Total: UAH {getTotalPrice()}</h2>*/}
                     <button className='w-[300px] m-auto bg-[#CA0123] hover:text-[#660000] text-white text-2xl p-4 rounded-3xl' onClick={cleanCart}>Make an order</button>
                 </>
             )}
             </main>
-        </Provider>
+        // </Provider>
     );
 }
 
